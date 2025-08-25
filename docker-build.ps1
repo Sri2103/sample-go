694657e7d5975a5a12b158032783a5b8009f8402
Sri2103 <damasriharsha@gmail.com>
2025-08-25 11:02:13 +0530

    add scripts for docker run

--- docker-build.ps1
+++ docker-build.ps1
@@ -3,9 +3,12 @@
 
 $ImageName = "jenkins:local"
 $DockerFile = "DockerJenkins"
-$DockerGroupId = 998   # <-- change this if your host's docker group GID is different
+#$DockerGroupId = 998   # <-- change this if your host's docker group GID is different
 
 Write-Host "Building custom Jenkins image..."
-docker build --build-arg DOCKER_GID=$DockerGroupId -t $ImageName $DockerFile .
+
+docker rmi $ImageName
+# docker build --build-arg DOCKER_GID=$DockerGroupId -t $ImageName $DockerFile .
+docker build -t $ImageName -f $DockerFile .
 
 Write-Host "Custom Jenkins image '$ImageName' built successfully."
